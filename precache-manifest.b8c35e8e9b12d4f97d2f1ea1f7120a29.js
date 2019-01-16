40a28caface789bfa848163b9d1585d955096da7
liskieero <liskieero@gmail.com>
2019-01-16 13:42:12 +0200

    Updates

---
+++ precache-manifest.b8c35e8e9b12d4f97d2f1ea1f7120a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "f30b187b7f923f96a872",
+    "url": "/react-gh-pages/static/js/main.f30b187b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-gh-pages/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f30b187b7f923f96a872",
+    "url": "/react-gh-pages/static/css/main.5e75823f.chunk.css"
+  },
+  {
+    "revision": "0ce2e084a5f88bd5624a8d653695b67f",
+    "url": "/react-gh-pages/index.html"
+  }
+];
